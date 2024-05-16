e04aa4d9f1f37662cf5a807e4696f5529a032665
Utkarsh Sharma <Vishal30982@gmail.com>
2024-05-16 22:00:46 +0530

    Some Improvement I think swipe gesture and collision. I really don't know because I opened this project after a long time and saw it's not committed so.. I commit it

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const char = document.querySelector('#character');
-const fence = document.querySelector('#fence');
-const score = document.querySelector('#score #scoreCount');
-const jumpSound = new Audio('./audios/jump-sound.mp3');
-const success = new Audio('./audios/success.mp3');
-const gameOverSound = new Audio('./audios/game-over.mp3');
-const bgMusic = new Audio('./audios/bg-music.mp3');
-bgMusic.autoplay = true;
-bgMusic.volume = 0.1;
+document.addEventListener('DOMContentLoaded', () => {
+    const char = document.querySelector('#character');
+    const pole = document.querySelector('#pole');
+    const score = document.querySelector('#score #scoreCount');
+    let touchStartY = 0;
+    let touchEndY = 0;
+    let isSwiped = () => {
+        if (touchEndY < touchStartY) {
+            jump();
+        }
+    };
+    const jumpSound = new Audio('./audios/jump-sound.mp3');
+    const success = new Audio('./audios/success.mp3');
+    const gameOverSound = new Audio('./audios/game-over.mp3');
+    const bgMusic = new Audio('./audios/bg-music.mp3');
+    bgMusic.autoplay = true;
+    bgMusic.volume = 0.1;
 
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'ArrowUp' || e.code === 'Space') {
+    function jump() {
         char.classList.add('jump');
         jumpSound.play();
         setTimeout(() => {
@@ -19,51 +26,43 @@ document.addEventListener('keydown', (e) => {
             success.play();
         }, 1000);
     }
-})
-// setInterval(() => {
-//     // left right collision detection
-//     if (parseInt(window.getComputedStyle(fence).getPropertyValue('left')) <= parseInt(window.getComputedStyle(char).getPropertyValue('left')) + char.width && parseInt(window.getComputedStyle(char).getPropertyValue('bottom') < fence.height )) {
-//         console.log('left right collision!!')
-//     }
-//     // top bottom collision detection
-//     if (parseInt(window.getComputedStyle(char).getPropertyValue('bottom')) <= parseInt(window.getComputedStyle(fence).getPropertyValue('bottom')) + fence.height && parseInt(window.getComputedStyle(char).getPropertyValue('bottom')) > 241) {
-//         console.log('top bottom collision!!')
-//     }
-    
-//     // todo: top botom collision detection
-// }, 1);
 
-let intervalId;
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'ArrowUp' || e.code === 'Space') {
+            jump()
+        }
+    })
 
-function checkCollision(elem1, elem2) {
-    const rect1 = elem1.getBoundingClientRect();
-    const rect2 = elem2.getBoundingClientRect();
+    document.addEventListener('touchstart', (event) => {
+        touchStartY = event.changedTouches[0].screenY;
+    })
+    document.addEventListener('touchend', (event) => {
+        touchEndY = event.changedTouches[0].screenY;
+        isSwiped()
+    })
 
-    return !(rect1.right < rect2.left ||
-             rect1.left > rect2.right ||
-             rect1.bottom < rect2.top ||
-             rect1.top > rect2.bottom);
-}
+    let intervalId;
 
-function gameLoop() {
-    if (checkCollision(char, fence)) {
-        bgMusic.pause()
-        bgMusic.currentTime = 0;
-        gameOverSound.play();
-        alert(`Oops! You hit the pole!\nYour Score: ${score.textContent}\n\nclick ok to restart`);
-        location.reload();
-        clearInterval(intervalId);
+    function checkCollision(elem1, elem2) {
+        const rect1 = elem1.getBoundingClientRect();
+        const rect2 = elem2.getBoundingClientRect();
+
+        return !(rect1.right < rect2.left ||
+                rect1.left > rect2.right ||
+                rect1.bottom < rect2.top ||
+                rect1.top > rect2.bottom);
     }
-}
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowUp') {
-        char.classList.add('jump');
-        setTimeout(() => {
-            char.classList.remove('jump');
-        }, 1000);
+    function gameLoop() {
+        if (checkCollision(char, pole)) {
+            bgMusic.pause()
+            bgMusic.currentTime = 0;
+            gameOverSound.play();
+            alert(`Oops! You hit the pole!\nYour Score: ${score.textContent}\n\nclick ok to restart`);
+            location.reload();
+            clearInterval(intervalId);
+        }
     }
+
+    intervalId = setInterval(gameLoop, 50);
 });
-
-
-intervalId = setInterval(gameLoop, 50);
